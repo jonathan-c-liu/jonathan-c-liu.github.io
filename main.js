a67c2da067b20a2512c62659b527631f4d612a2c
Jonathan Liu <jonathancliu168@gmail.com>
2021-06-07 19:00:29 -0500

    site redo!

--- main.js
+++
@@ -1,9 +0,0 @@
-const show_nav_bar = () => {
-    nav_bar = document.getElementById('nav_bar_mobile');
-
-    if (nav_bar.style.display != "flex"){
-        nav_bar.style.display = "flex"
-    } else {
-        nav_bar.style.display = "none"
-    }
-}
